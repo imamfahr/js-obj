e3ad0402a831cd02cd6b462806bfebb41b21893b
imamfahr <imamfahr@gmail.com>
2020-08-30 13:06:21 +0800

    styling 1

--- script.js
+++ script.js
@@ -3,25 +3,57 @@ let fruits = [
         name : "Apple",
         scientificName:"Malus domestica",
         clade: "Dicotyl",
-    }
+        img: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
     {
         name: "Banana",
         scientificName: "Musa paradisiaca",
         clade : "Monocotyl",
-    }
+        img: "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
     {
         name: "Blueberry",
         scientificName: "Vaccinum corybosum",
         clade : "Dicotyl",
-    }
+        img: "https://images.unsplash.com/photo-1498557850523-fd3d118b962e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
     {
         name: "Cherry",
         scientificName: "Prunus avium",
         clade : "Dicotyl",
-    }
+        img:"https://images.unsplash.com/photo-1528820600606-0ef5600cbfee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
     {
-        name: "Cherry",
-        scientificName: "Prunus avium",
+        name: "Melon",
+        scientificName: "Cucumis metuliferus",
         clade : "Dicotyl",
-    }
+        img:"https://images.unsplash.com/photo-1582332205846-bf7627cae190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+        name: "Lemon",
+        scientificName: "Citrus limon",
+        clade : "Dicotyl",
+        img:"https://images.unsplash.com/photo-1568569350062-ebfa3cb195df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+        name: "Mango",
+        scientificName: "Mangifera indica",
+        clade : "Dicotyl",
+        img:"https://images.unsplash.com/photo-1591073113125-e46713c829ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+        name: "Orange",
+        scientificName: "Citrus sinesis",
+        clade : "Dicotyl",
+        img:"https://images.unsplash.com/photo-1582979512210-99b6a53386f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+        name: "Grape",
+        scientificName: "Vitis vinifera",
+        clade : "Dicotyl",
+        img:"https://images.unsplash.com/photo-1423483641154-5411ec9c0ddf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
 ]
+
+console.log(fruits[2].name)
+
