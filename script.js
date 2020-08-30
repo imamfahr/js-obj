9b9ee351694441ba5f37fcb165b96b91b376611f
imamfahr <imamfahr@gmail.com>
2020-08-30 09:47:46 +0800

    add files + json

---
+++ script.js
@@ -0,0 +1,27 @@
+let fruits = [
+    {
+        name : "Apple",
+        scientificName:"Malus domestica",
+        clade: "Dicotyl",
+    }
+    {
+        name: "Banana",
+        scientificName: "Musa paradisiaca",
+        clade : "Monocotyl",
+    }
+    {
+        name: "Blueberry",
+        scientificName: "Vaccinum corybosum",
+        clade : "Dicotyl",
+    }
+    {
+        name: "Cherry",
+        scientificName: "Prunus avium",
+        clade : "Dicotyl",
+    }
+    {
+        name: "Cherry",
+        scientificName: "Prunus avium",
+        clade : "Dicotyl",
+    }
+]
